22c179032be247599cf986407a797eda97083f17
jivan aryal <jeevanaryal7947@gmail.com>
2023-01-03 16:32:12 +0545

    responsive footer

--- src/Components/Pages/HomePages/Footer.jsx
+++ src/Components/Pages/HomePages/Footer.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-[#000000]">
-      <div className="flex justify-around  text-white py-8">
+      <div className="w-11/12 mx-auto grid lg:grid-cols-4 md:grid-cols-3 pd:grid-cols-2 text-white py-8">
         <div className="first-item">
           <div className="text-3xl ">Links</div>
-          <div className="capitalize flex flex-col gap-2 py-2">
+          <div className="capitalize flex flex-col gap-2 py-2 text-sm">
             <p>home</p>
             <p>About Us</p>
             <p>Our Services</p>
@@ -17,7 +17,7 @@ const Footer = () => {
         </div>
         <div className="second-item">
           <div className="text-3xl ">About</div>
-          <div className="capitalize flex flex-col gap-2 py-2">
+          <div className="capitalize flex flex-col gap-2 py-2 text-sm">
             <p>Company</p>
             <p>Careers</p>
             <p>Customers</p>
@@ -25,17 +25,17 @@ const Footer = () => {
           </div>
         </div>
         <div className="third-item">
-          <div className="text-3xl ">Featured Courses</div>
-          <div className="capitalize flex flex-col gap-2 py-2">
+          <div className="text-3xl  ">Featured Courses</div>
+          <div className="capitalize flex flex-col gap-2 py-2 text-sm">
             <p>MERN Stack Development</p>
             <p> Starts In March 20,2020</p>
             <p>Web Designing</p>
             <p>Starts In April 21,2020</p>
           </div>
         </div>
-        <div className="fourth-item">
+        <div className="lg:pl-6 md:pt-4">
           <div className="text-3xl ">Our Contact</div>
-          <div className="capitalize flex flex-col gap-2 py-2">
+          <div className="capitalize flex flex-col gap-2 py-2 text-sm">
             <p>+977-071532805,9827494116,9857045414</p>
             <p> training@hubit.com.np</p>
             <p>Butwal-09 (Ganesh Path),Milanchowk Rupandehi Nepal</p>
